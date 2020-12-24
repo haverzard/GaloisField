52829772edbc08c6f779f70b77f8dff621776b7f
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-12-24 11:08:33 +0700

    Add negative degree exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,3 +2,8 @@ class FFOperationException(Exception):
     def __init__(self, op="", msg=""):
         self.operator = op
         self.message = msg
+
+
+class FPNegativeDegreeNotAllowed(Exception):
+    def __init__(self):
+        self.message = "Negative degree polynom is not allowed"
